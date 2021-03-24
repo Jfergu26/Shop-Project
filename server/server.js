81d6761b2f465276cebc5434c345d304f952c2f7
Jaryntf <jaryn.ferguson@macys.com>
2021-03-24 18:30:50 -0400

    Client and server working together

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+let cors = require('cors');
+
 
 const app = express();
-const port = 3000;
+const port = 9000;
 
+app.use(cors());
+app.get('/Login', (req,res)=>{
+    res.send(`user not logged in`)
+})
 app.listen(port, () =>{
     console.log(`Listening at ${port}`)
 })
